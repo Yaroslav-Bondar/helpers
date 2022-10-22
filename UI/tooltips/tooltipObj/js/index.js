9214b73da40f446e48d4c3e9b5324b18b96a635c
Yaroslav-Bondar <jamnetbox@gmail.com>
2022-10-23 00:31:30 +0300

    the ability to show styles, accounting for tooltip objects

--- UI/tooltips/tooltipObj/js/index.js
+++ UI/tooltips/tooltipObj/js/index.js
@@ -5,42 +5,37 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify
 // https://jsfiddle.net/Yaroslav_Bondar/gkp9u5qc/3/
 
-function Tooltip(targetClassName, content = 'I am tooltip', position = 'top', styles = null) {
-	this._target = document.getElementsByClassName(targetClassName)[0];
+// https://developer.mozilla.org/en-US/docs/Web/API/CSSStyleDeclaration/setProperty
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/delete
+// https://learn.javascript.ru/weakmap-weakset
+// https://dev.to/rahmanfadhil/how-to-generate-unique-id-in-javascript-1b13
+// https://stackoverflow.com/questions/28814585/how-to-check-if-type-is-boolean
+// {
+//     [Tooltip.generateId()]: new Tooltip('element', 'Hello I am Tooltip', 'left'),
+
+// }
+
+
+function Tooltip(targetClassName, content = 'I am tooltip', position = 'top', styles = null, archive) {
+    this._target = document.getElementsByClassName(targetClassName)[0];
     this._content = content; 
     this._position = position;
-    this._styles = styles;
+    // this._styles = styles;
     this._constructor = Object.getPrototypeOf(this).constructor;
-    this._positions = new Set(['top', 'left', 'right', 'bottom']); // possible implementation as a static property
-
-    Object.defineProperties(this, {
-        positioning: {
-            get: function() {
-                return this._position;
-            },
-            set: function(value) {
-                // check current entered position
-                if(this._position === value) {
-                    console.log('Warning: Current position is equivalent to given position !!!');
-                    return false;
-                };
-                // Check the entered keyword desrcribing the position.
-                if(!this._positions.has(value)) {
-                    console.log("Warning: Invalid position value: must be a string of 'top','left','right','bottom' !!!");
-                    return false;
-                };
-                this._tooltipItem.classList.replace(`tooltip__item_position_${this._position}`, `tooltip__item_position_${value}`);
-                this._position = value;
-                return true;
-            },
-            
-        },
-    });
+    this.id = this._constructor.uid();
+    this._constructor.archive[this.id] = this;
+    this._styles = styles ? this._constructor.styleHyphenFormat(styles) : styles;
+    // this._positions = new Set(['top', 'left', 'right', 'bottom']); // move it to the prototype !!!
     this._setTooltip();
 }
+// static properties
+// Tooltip.tooltipArchive = new WeakMap();
+Tooltip.archive = Object.create(Object.prototype);
 // static methods
+Tooltip.uid = () => String(Date.now().toString(36) + Math.random().toString(16)).replace(/\./g, '');
 Tooltip.upperToHyphenLower = upper => '-' + upper.toLowerCase();
-
+Tooltip.styleToMultiline = style => style.replace(/;/g, ';\n');
 Tooltip.styleHyphenFormat = function(styles) {
     let str = JSON.stringify(styles);
     let output = str.replace(/[{"}]/g, '')
@@ -48,10 +43,41 @@ Tooltip.styleHyphenFormat = function(styles) {
                         .replace(/[A-Z]/g, this.upperToHyphenLower) + ';';
     return output;
 }
-// method prototypes
+// prototype getters/setters
+Object.defineProperties(Tooltip.prototype, {
+    currentPosition: {
+        get: function() {
+            console.log(`Position value is ${this._position}`);
+            return this._position;
+        },
+        set: function(value) {
+            // check current entered position
+            if(this._position === value) {
+                console.log(`Position change warning: Current position - ${this._position} is equivalent to given position - ${value} !!!`);
+            };
+            // Check the entered keyword desrcribing the position.
+            if(!this._positions.has(value)) {
+                let positions = '';
+                this._positions.forEach(value => positions += `${value} `);
+                console.log(`Position change warning: Invalid position value. Must be a string containing one of the values: ${positions}`);
+            };
+            this._tooltipItem.classList.replace(`tooltip__item_position_${this._position}`, `tooltip__item_position_${value}`);
+            this._position = value;
+        },
+        
+    },
+    // available positions getter
+
+});
+// prototype properties
+Tooltip.prototype._positions = new Set(['top', 'left', 'right', 'bottom']);
+// Tooltip.prototype._classes = {
+//     tooltipItem: 'tooltip__item',
+// }
+// prototype methods
 Tooltip.prototype._setTooltip = function () {
     const tooltipContainer = document.createElement('div');
-    const tooltipHtml = `<div class='tooltip__item tooltip__item_position_${this._position}'>
+    const tooltipHtml = `<div id=${this.id} class='tooltip__item tooltip__item_position_${this._position}'>
                             ${this._content}
                         </div>
                         <div class='tooltip__target'></div>`;
@@ -67,16 +93,39 @@ Tooltip.prototype._setTooltip = function () {
     tooltipTarget.onmouseenter = this._showTooltip.bind(this);
     tooltipTarget.onmouseleave = this._hideTooltip.bind(this);
     // set styles
-    this.setStyles(this._styles);
+    if(this._styles) this._tooltipItem.style.cssText = this._styles;
+    // this.setStyles(this._styles);
 }
-Tooltip.prototype.setStyles = function(styles) {
+// Tooltip.prototype.setStyles = function(styles) {
+//     if(this._styles) {
+//         const stylesString = this._constructor.styleHyphenFormat(this._styles);
+//         this._tooltipItem.style.cssText = stylesString;
+//         return true;
+//     }
+//     console.log('styles are not defined, default styles will be defined');
+//     return false;
+// }
+Tooltip.prototype.showStyles = function(allSheets = false) {
     if(this._styles) {
-        const stylesString = this._constructor.styleHyphenFormat(this._styles);
-        this._tooltipItem.style.cssText = stylesString;
-        return true;
+        console.log('this._styles\n', this._constructor.styleToMultiline(this._styles));
+        
+    } else {
+        console.log('custom styles are not defined', this._styles);
     }
-    console.log('styles are not defined, default styles will be defined');
-    return false;
+    console.log('CSS styles:');
+    for(const sheet of document.styleSheets) {
+        const rules = sheet.cssRules;
+        const targetRules = [...rules].filter(({selectorText}) => /^\.tooltip__item(_[a-z]+){0,2}$/.test(selectorText));
+        if(targetRules.length) {
+            const {href, ownerNode} = sheet;
+            console.log('ownerNode', ownerNode);
+            console.log('href', href);
+            targetRules.forEach(rule => {
+                console.log('cssText', rule.cssText);
+            });
+            if(!allSheets) break;
+        }; 
+    }
 }
 Tooltip.prototype._showTooltip = function() {
     this._tooltipItem.classList.add('tooltip__item_active');
@@ -85,8 +134,13 @@ Tooltip.prototype._hideTooltip = function () {
     this._tooltipItem.classList.remove('tooltip__item_active');
 }
 
+// instances
+
 const tooltip = new Tooltip('element', 'Hello I am Tooltip', 'left');
-tooltip.positioning = 'bottom';
+// console.log(tooltip.currentPosition = 'bottom');
+// tooltip.currentPosition;
+// console.log('tooltip currentPosition', tooltip.currentPosition);
+// tooltip.showStyles();
 // console.log(tooltip);
 const tooltip2 = new Tooltip('element2', 'Hello I am Tooltip 2', 'right', {
         backgroundColor: 'green', 
@@ -96,9 +150,11 @@ const tooltip2 = new Tooltip('element2', 'Hello I am Tooltip 2', 'right', {
         left: 0, 
     }); 
 //console.log(tooltip2);
-tooltip2.positioning = 'top';
-tooltip2.positioning = 'lefte';
-//console.log(tooltip2.positioning);
+tooltip2.currentPosition = 'top';
+tooltip2.currentPosition = 'lefte';
+console.log('tooltip2.id', tooltip2.id);
+// tooltip2.showStyles();
+//console.log(tooltip2.currentPosition);
 // inheritance
 function TooltipExt(targetClassName, content = 'I am tooltip extended', position = 'top', styles = {
         borderLeft: '2px solid red', color: 'green',
@@ -108,6 +164,7 @@ function TooltipExt(targetClassName, content = 'I am tooltip extended', position
     // call parents constructor
     Tooltip.call(this, targetClassName, content, position, styles);
 }
+console.log('archive', Tooltip.archive);
 // protype method inheritance 
 Object.setPrototypeOf(TooltipExt.prototype, Tooltip.prototype);
 // static method inheritance
@@ -115,7 +172,25 @@ Object.setPrototypeOf(TooltipExt, Tooltip); // or: TooltipExt.__proto__ = Toolti
 const tooltipExt = new TooltipExt('element3');
 // console.log('tooltipExt', tooltipExt);
 // console.log('TooltipExt.__proto__:', TooltipExt.__proto__);
-tooltipExt.positioning = 'left';
+tooltipExt.currentPosition = 'left';
+// tooltipExt.showStyles();
 // console.log('TooltipExt', TooltipExt);
-// console.log('tooltipExt.positioning', tooltipExt.positioning);
+// console.log('tooltipExt.currentPosition', tooltipExt.currentPosition);
 // console.log('tooltipExt.constructor', tooltipExt.constructor);
+
+var styleObj = document.styleSheets[0].cssRules[0].style;
+
+// for(const sheet of document.styleSheets) {
+//     // console.log('sheet', Array.isArray(sheet));
+//     console.log('sheet', sheet);
+//     const rules = sheet.cssRules;
+//     const target = rules.find(rule => rule.selectorText === {}) 
+// }
+// console.log(styleObj.cssText);
+
+// for (var i = styleObj.length; i--;) {
+//   var nameString = styleObj[i];
+//   styleObj.removeProperty(nameString);
+// }
+
+// console.log(styleObj.cssText);
