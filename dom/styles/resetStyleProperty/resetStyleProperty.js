23d416fdf16b3ac4cc26e2f7057d7c2f90d4e3e8
Yaroslav-Bondar <jamnetbox@gmail.com>
2022-12-01 14:58:08 +0200

    function to reset a style property on a DOM element

---
+++ dom/styles/resetStyleProperty/resetStyleProperty.js
@@ -0,0 +1,15 @@
+/**
+ * Reset style property on DOM element.
+ * @param {string} className - target element class name.
+ * @param {string} property - property name.
+ * @returns {function} - anonymous function to reset a style property on a DOM element.
+ */
+ function resetStyleProperty(className, property) {
+    const element = document.querySelector(`.${className}`);
+    return function() {
+        if(element.style[property]) {
+            console.log('reset element style property');
+            element.style[property] = '';
+        }  
+    }
+}
